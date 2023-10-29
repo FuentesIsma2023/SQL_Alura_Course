066df2825d8844ddd189dbf9c830cbf81602b6be
Ismael Najera <ismaelnajerafuentes@gmail.com>
2023-10-29 16:12:42 -0600

    Creando nuevas Queries

---
+++ Course_2/Usando Order By.sql
@@ -0,0 +1,7 @@
+SELECT * FROM tabla_de_productos;
+
+SELECT * FROM tabla_de_productos ORDER BY PRECIO_DE_LISTA;
+
+SELECT * FROM tabla_de_productos ORDER BY PRECIO_DE_LISTA;
+
+SELECT * FROM tabla_de_productos ORDER BY NOMBRE_DEL_PRODUCTO;
